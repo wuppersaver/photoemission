877af1691e1a8d79eccd3cbc47e17a5a97f0969c
Felix Mildner <felix@wuppersaver.de>
2022-12-12 20:26:17 +0000

    Changed functionality in od_output_class And added some functions in Plotting.ipynb

--- optados_output_class.py
+++ optados_output_class.py
@@ -7,71 +7,72 @@ class OptaDOSOutput:
         scf_lines = []
         self.path = path
         #self.seed = path.split('/')[-1].split('.')[0]
-        lattice = [[],[],[]]
-        atoms,atom_coordinates = [],[]
+        self.lattice = [[],[],[]]
+        self.atoms,self.atom_coordinates = [],[]
         self.layers= {}
         optados_jobs = ['output_DOS', 'output_partial_DOS', 'output_projected_BS', 'output_jDOS', 'output_optic_response', 'output_corelvl_spec', 'photoemission']
         self.methods = []
+        lines = None
         with open(path,'r') as f:
-            for line in f:
-                line = line.strip()
-                if 'JOB CONTROL' in line:
-                    for i in range(7):
-                        line = next(f)
-                        #print(line)
-                        if 'True' in line:
-                            self.methods.append(optados_jobs[i])
-                    #print(next(f).strip().split()[4])
-                    self.iprint = int(next(f).strip().split()[4])
-                    if 'True' in next(f): self.file_format = 'old'
-                    else: self.file_format = 'new'
-                if 'Smearing Width' in line:
-                    self.temperature = float(line.split()[4])*11604.51812
-                if 'Electronic Data' in line:
-                    self.num_bands = int(next(f).strip().split()[5])
-                    temp = next(f).strip().split()
-                    if '***' not in temp:    
-                        self.k_grid = [int(temp[4]),int(temp[6]),int(temp[8])]
-                    self.num_kpts = int(next(f).strip().split()[5])
-                    if 'True' in next(f).strip(): self.spin_polarised = True
-                    else: self.spin_polarised = False
-                    self.num_electrons = float(next(f).strip().split()[5])
+            lines = f.readlines()            
+        for idx,line in enumerate(lines):
+            line = line.strip()
+            if 'JOB CONTROL' in line:
+                for i in range(7):
+                    if 'True' in lines[idx+1+i]:
+                        self.methods.append(optados_jobs[i])
+                #print(next(f).strip().split()[4])
+                self.iprint = int(lines[idx+8].strip().split()[4])
+                if 'True' in lines[idx+9]: self.file_format = 'old'
+                else: self.file_format = 'new'
+            if 'Smearing Width' in line:
+                self.temperature = float(line.split()[4])*11604.51812
+            if 'Electronic Data' in line:
+                self.num_bands = int(lines[idx+1].strip().split()[5])
+                temp = lines[idx+2].strip().split()
+                if '***' not in temp:    
+                    self.k_grid = [int(temp[4]),int(temp[6]),int(temp[8])]
+                self.num_kpts = int(lines[idx+3].strip().split()[5])
+                if 'True' in lines[idx+4].strip(): self.spin_polarised = True
+                else: self.spin_polarised = False
+                self.num_electrons = float(lines[idx+5].strip().split()[5])
 
-                if 'Projected Density Of States Calculation' in line:
-                    #print('fermi analysis')
-                    for i in range(6):
-                        line = next(f)
-                    line = line.strip().split()
-                    #print(line)
-                    self.fermi_e = float(line[6])
+            if 'Projected Density Of States Calculation' in line:
+                #print('fermi analysis')
+                for i in range(6):
+                    line = next(f)
+                line = lines[idx+6].strip().split()
+                #print(line)
+                self.fermi_e = float(line[6])
 
-                if 'Max number of atoms' in line:
-                    self.max_atoms = int(line.split()[5])
-                    self.max_layers = int(line.split()[10])
-                if 'Work Function' in line:
-                    self.work_fct = float(line.split()[3])
-                    self.photon_e = float(line.split()[7])
-                    temp = next(f).strip().split()
-                    self.work_fct_effect = float(temp[4])
-                    self.e_field = float(temp[8].replace('V/A',''))
-                    temp = next(f)
-                    if 'Free electron state' in temp: self.final_state = 'free electron'
-                    if 'Bloch state' in temp: self.final_state = 'bloch'
-                    #print(self.final_state)
-                if 'Quantum Efficiency' in line:
-                    #print(self.max_layers)
-                    for i in range(self.max_layers):
-                        temp = next(f).strip().split()
-                        self.layers[str(i)] = [temp[1], int(temp[2]), int(temp[3])]
-                if 'Total quantum efficiency' in line:
-                    self.qe = float(line.split()[5])
-                    temp = next(f).strip()
-                    if 'NaN' in temp: self.mte = 0
-                    else: self.mte = float(temp.split()[6])
-                if 'Lattice Vectors' in line:
-                    for i in range(3):
-                        temp = next(f).strip().split()
-                        lattice[i] = [float(x) for x in temp[1:4]]
+            if 'Max number of atoms' in line:
+                self.max_atoms = int(line.split()[5])
+                self.max_layers = int(line.split()[10])
+            if 'Work Function' in line and not 'Effective' in line:
+                self.work_fct = float(line.split()[3])
+                self.photon_e = float(line.split()[7])
+                temp = lines[idx+1].strip().split()
+                #print(temp)
+                self.work_fct_effect = float(temp[4])
+                self.e_field = float(temp[8].replace('V/A',''))
+                temp = lines[idx+1]
+                if 'Free electron state' in temp: self.final_state = 'free electron'
+                if 'Bloch state' in temp: self.final_state = 'bloch'
+                #print(self.final_state)
+            if '  Quantum Efficiency  ' in line:
+                #print(self.max_layers)
+                for i in range(self.max_layers):
+                    temp = lines[idx+1+i].strip().split()
+                    self.layers[str(i)] = [temp[1], int(temp[2]), int(temp[3])]
+            if 'Total Quantum Efficiency' in line:
+                self.qe = float(line.split()[5])
+                self.mte = float(lines[idx+1].split()[6])
+            if 'Lattice Vectors' in line:
+                for i in range(3):
+                    self.lattice[i] = [float(x) for x in lines[idx+1+i].strip().split()[1:]]
+
+            if self.iprint > 1 and 'jdos_max_energy' in line:
+                self.jdos_max_energy = float(line.strip().split()[3])
     
     def create_bandstructure(self,):
         
